a40e192b406ca85af14a3ecb2ce94adccfc68b4f
houssamtrizi <houssam.trizi@gmail.com>
2018-06-06 22:42:50 +0000

    initial commit

--- cpp_training/main.cpp
+++ cpp_training/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, i am new to c++ so please bear with me!\n";
     return 0;
 }
