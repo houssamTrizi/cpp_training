8bf9593e90a5536113f1c7288747ada5d9c20361
houssamtrizi <houssam.trizi@gmail.com>
2018-06-06 22:35:46 +0000

    Initial Commit

---
+++ cpp_training/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_training
+//
+//  Created by Houssam Eddine TRIZI on 6/6/18.
+//  Copyright © 2018 Houssam Eddine TRIZI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
